8389786eb97174c272fd4920fb5578996dd282f8
Fabio <fabio.cufino5@gmail.com>
2021-11-03 12:39:24 +0100

    fine circuito 2 e inizio 3

--- Esperienza_02/root/circ_3.cpp
+++ Esperienza_02/root/circ_3.cpp
@@ -1,5 +1,18 @@
+#include "TH1.h"
+#include "TMath.h"
+#include "TF1.h"
+#include "TLegend.h"
+#include "TCanvas.h"
+#include <fstream>
 #include <cmath>
+#include "TFitResult.h"
+#include "TMatrixD.h"
+#include "TVectorD.h"
+#include "TGraphErrors.h"
+#include <vector>
+#include "root.h"
 
+#include "DATACLASSGEN.h"
 using namespace std;
 
 double err_res_cap(double resistenza, double perc, double digit, double lsd)
@@ -8,7 +21,26 @@ double err_res_cap(double resistenza, double perc, double digit, double lsd)
     double read = digit * lsd * 1. / sqrt(3);
     return sqrt(pow(gain, 2) + pow(read, 2));
 }
-int main()
+
+enum class Colonna : int
+{
+    t = 0,
+    fst = 1,
+    v_out = 2,
+    fs_vout = 3
+};
+
+double somma_seni(double *x, double *par)
+{
+    double a = par[0];
+    double b = par[1];
+    double c = par[2];
+
+    double val = 1 * sin(2 * M_PI * 1000 * x[0] + a) + 1 * sin(2 * M_PI * 2000 * x[0] + b) + c;
+    return val;
+}
+
+void circ_3()
 {
     double r1 = 9.993e3;     //ohm
     double r1b = 10.003e3;   //ohm
@@ -22,5 +54,45 @@ int main()
     double err_r50 = err_res_cap(r50, 0.1, 8, 0.01);     //ohm
     double err_r50b = err_res_cap(r50b, 0.1, 8, 0.01);   //ohm
 
-    return 0;
+    DataContainerGen tre;
+    tre.read("../Dati/circuito_3.txt", 4);
+    vector<double> &t = tre.tabella[(int)Colonna::t];
+    vector<double> &v_out = tre.tabella[(int)Colonna::v_out];
+    vector<double> &fs_vout = tre.tabella[(int)Colonna::fs_vout];
+
+    vector<double> err_vout;
+    tre.err_oscilloscopio((int)Colonna::fs_vout, (int)Colonna::v_out, err_vout);
+    vector<double> err_tempo(err_vout.size(), 0);
+
+    // ------------------------------------------INIZIO GRAFCIO E CALCOLO PARAMETRI-- -------------
+    auto canvas1 = new TCanvas("c1", "Circuito 1", 1000, 600);
+    canvas1->SetGrid();
+    canvas1->SetFillColor(0);
+
+    TGraphErrors *fileInput = new TGraphErrors(into_root(t), into_root(v_out), into_root(err_tempo), into_root(err_vout));
+
+    fileInput->SetMarkerColor(4);
+    fileInput->SetMarkerColor(kAzure - 2);
+    fileInput->SetMarkerStyle(20);
+    fileInput->SetMarkerSize(0.7);
+    fileInput->SetTitle("");
+    fileInput->GetXaxis()->SetTitle("t [us]");
+    fileInput->GetYaxis()->SetTitle("V_{out} [mV]");
+    fileInput->GetXaxis()->SetAxisColor(14);
+    fileInput->GetYaxis()->SetAxisColor(14);
+
+    fileInput->Draw("AP");
+
+    TF1 *fit_sin = new TF1("f2", somma_seni, 0, 1000, 3);
+    fit_sin->SetLineColor(kRed);
+    fit_sin->SetLineStyle(2);
+    fit_sin->SetLineWidth(1);
+
+
+    fit_sin->SetParameter(0, 1);
+    fit_sin->SetParameter(1, 1);
+    fit_sin->SetParameter(2, 1);
+
+    fit_results fitres;
+    fit(fit_sin, 3, fileInput, t, v_out, fitres, -10, 1000);
 }
--- Esperienza_02/root/circuito_1.cxx
+++ Esperienza_02/root/circuito_1.cxx
@@ -17,152 +17,152 @@ using namespace std;
 
 double err_res_cap(double resistenza, double perc, double digit, double lsd)
 {
-    double gain = perc / 100. * resistenza * 1. / sqrt(3);
-    double read = digit * lsd * 1. / sqrt(3);
-    return sqrt(pow(gain, 2) + pow(read, 2));
+     double gain = perc / 100. * resistenza * 1. / sqrt(3);
+     double read = digit * lsd * 1. / sqrt(3);
+     return sqrt(pow(gain, 2) + pow(read, 2));
 }
 
 enum class Colonna : int
 {
-    vin = 0,
-    fs_vin = 1,
-    vout = 2,
-    fs_vout = 3
+     vin = 0,
+     fs_vin = 1,
+     vout = 2,
+     fs_vout = 3
 };
 
 void circuito_1()
 {
-    // Analisi capacità
-    double c1 = 99.5;                           //nanofarad
-    double c2 = 111.0;                          //nanofarad
-    double err_c1 = err_res_cap(c1, 1, 8, 0.1); //nanofarad
-    double err_c2 = err_res_cap(c2, 1, 10, 1);  //nanofarad
-    cout << "Capacità [nanofarad]:" << endl
-         << c1 << " +- " << err_c1 << endl
-         << c2 << " +- " << err_c2 << endl
-         << endl;
+     // Analisi capacità
+     double c1 = 99.5;                           //nanofarad
+     double c2 = 111.0;                          //nanofarad
+     double err_c1 = err_res_cap(c1, 1, 8, 0.1); //nanofarad
+     double err_c2 = err_res_cap(c2, 1, 10, 1);  //nanofarad
+     cout << "Capacità [nanofarad]:" << endl
+          << c1 << " +- " << err_c1 << endl
+          << c2 << " +- " << err_c2 << endl
+          << endl;
 
-    // Analisi delle resistenze
-    double r1 = 9.993e3;                             //ohm
-    double rf = 67.759e3;                            //ohm
-    double r50 = 46.51;                              //ohm
-    double err_r1 = err_res_cap(r1, 0.07, 8, 1);     //ohm
-    double err_rf = err_res_cap(rf, 0.07, 8, 1);     //ohm
-    double err_r50 = err_res_cap(r50, 0.1, 8, 0.01); //ohm
-    double ampl_th = -rf / r1;
-    double err_ampl_th = sqrt(pow(err_rf / r1, 2) + pow((err_r1 * rf / pow(r1, 2)), 2));
-    cout << "Resistenze [ohm]:" << endl
-         << r1 << " +- " << err_r1 << endl
-         << rf << " +- " << err_rf << endl
-         << r50 << " +- " << err_r50 << endl
-         << endl;
+     // Analisi delle resistenze
+     double r1 = 9.993e3;                             //ohm
+     double rf = 67.759e3;                            //ohm
+     double r50 = 46.51;                              //ohm
+     double err_r1 = err_res_cap(r1, 0.07, 8, 1);     //ohm
+     double err_rf = err_res_cap(rf, 0.07, 8, 1);     //ohm
+     double err_r50 = err_res_cap(r50, 0.1, 8, 0.01); //ohm
+     double ampl_th = -rf / r1;
+     double err_ampl_th = sqrt(pow(err_rf / r1, 2) + pow((err_r1 * rf / pow(r1, 2)), 2));
+     cout << "Resistenze [ohm]:" << endl
+          << r1 << " +- " << err_r1 << endl
+          << rf << " +- " << err_rf << endl
+          << r50 << " +- " << err_r50 << endl
+          << endl;
 
-    cout << "Amplificazione prevista:" << endl
-         << ampl_th << "+-" << err_ampl_th << endl
-         << endl;
+     cout << "Amplificazione prevista:" << endl
+          << ampl_th << "+-" << err_ampl_th << endl
+          << endl;
 
-    //Letura e grafici dati
-    DataContainerGen uno;
-    uno.read("../Dati/circuito_1.txt", 4);
-    vector<double> &vin = uno.tabella[(int)Colonna::vin];
-    vector<double> &vout = uno.tabella[(int)Colonna::vout];
+     //Letura e grafici dati
+     DataContainerGen uno;
+     uno.read("../Dati/circuito_1.txt", 4);
+     vector<double> &vin = uno.tabella[(int)Colonna::vin];
+     vector<double> &vout = uno.tabella[(int)Colonna::vout];
 
-    vector<double> &fs_vin = uno.tabella[(int)Colonna::fs_vin];
-    vector<double> &fs_vout = uno.tabella[(int)Colonna::fs_vout];
+     vector<double> &fs_vin = uno.tabella[(int)Colonna::fs_vin];
+     vector<double> &fs_vout = uno.tabella[(int)Colonna::fs_vout];
 
-    vector<double> err_vin;
-    vector<double> err_vout;
-    uno.err_oscilloscopio((int)Colonna::fs_vout, (int)Colonna::vout, err_vout);
-    uno.err_oscilloscopio((int)Colonna::fs_vin, (int)Colonna::vin, err_vin);
+     vector<double> err_vin;
+     vector<double> err_vout;
+     uno.err_oscilloscopio((int)Colonna::fs_vout, (int)Colonna::vout, err_vout);
+     uno.err_oscilloscopio((int)Colonna::fs_vin, (int)Colonna::vin, err_vin);
 
-    //------------------------------------------INIZIO GRAFCIO E CALCOLO PARAMETRI---------------
-    auto canvas1 = new TCanvas("c1", "Circuito 1", 1000, 600);
-    canvas1->SetGrid();
-    canvas1->SetFillColor(0);
+     //------------------------------------------INIZIO GRAFCIO E CALCOLO PARAMETRI---------------
+     auto canvas1 = new TCanvas("c1", "Circuito 1", 1000, 600);
+     canvas1->SetGrid();
+     canvas1->SetFillColor(0);
 
-    TGraphErrors *fileInput = new TGraphErrors(into_root(vin), into_root(vout), into_root(err_vin), into_root(err_vout));
+     TGraphErrors *fileInput = new TGraphErrors(into_root(vin), into_root(vout), into_root(err_vin), into_root(err_vout));
 
-    fileInput->SetMarkerColor(4);
-    fileInput->SetLineColor(kAzure - 4);
-    fileInput->SetMarkerStyle(20);
-    fileInput->SetMarkerSize(0.7);
-    fileInput->SetTitle("");
-    fileInput->GetXaxis()->SetTitle("Vpp_{IN} [mV]");
-    fileInput->GetYaxis()->SetTitle("Vpp_{OUT} [V]");
-    fileInput->GetXaxis()->SetAxisColor(14);
-    fileInput->GetYaxis()->SetAxisColor(14);
+     fileInput->SetMarkerColor(4);
+     fileInput->SetLineColor(kAzure - 4);
+     fileInput->SetMarkerStyle(20);
+     fileInput->SetMarkerSize(0.7);
+     fileInput->SetTitle("");
+     fileInput->GetXaxis()->SetTitle("Vpp_{IN} [mV]");
+     fileInput->GetYaxis()->SetTitle("Vpp_{OUT} [V]");
+     fileInput->GetXaxis()->SetAxisColor(14);
+     fileInput->GetYaxis()->SetAxisColor(14);
 
-    fileInput->Draw("AP");
+     fileInput->Draw("AP");
 
-    fit_results fit1;
-    TF1 *retta = new TF1("retta", "pol1", 0, 10);
-    retta->SetLineColor(kRed);
-    retta->SetLineStyle(2);
-    retta->SetLineWidth(2);
-    fit(retta, 2, fileInput, vin, vout, fit1, 0, 3800);
+     fit_results fit1;
+     TF1 *retta = new TF1("retta", "pol1", 0, 10);
+     retta->SetLineColor(kRed);
+     retta->SetLineStyle(2);
+     retta->SetLineWidth(2);
+     fit(retta, 2, fileInput, vin, vout, fit1, 0, 3800);
 
-    TPaveStats *info;
-    TText *scri;
-    informazioni_fit_lineare(info,
-                             scri,
-                             to_string(fit1.chi_square[0]),
-                             to_string(fit1.dof[0]),
-                             to_string(fit1.a[0]),
-                             to_string(fit1.err_a[0]),
-                             to_string(fit1.b[0]),
-                             to_string(fit1.err_b[0]),
-                             to_string(fit1.rho[0]),
-                             to_string(fit1.t_student[0]),
-                             to_string(fit1.sigma_post[0]),
-                             "Fit lineare sui dati");
+     TPaveStats *info;
+     TText *scri;
+     informazioni_fit_lineare(info,
+                              scri,
+                              to_string(fit1.chi_square[0]),
+                              to_string(fit1.dof[0]),
+                              to_string(fit1.a[0]),
+                              to_string(fit1.err_a[0]),
+                              to_string(fit1.b[0]),
+                              to_string(fit1.err_b[0]),
+                              to_string(fit1.rho[0]),
+                              to_string(fit1.t_student[0]),
+                              to_string(fit1.sigma_post[0]),
+                              "Fit lineare sui dati");
 
-    TLegend *legend = new TLegend(0.15, 0.15, 0.5, 0.3);
-    legend->AddEntry(fileInput, "Dati con errore", "P");
-    legend->AddEntry(retta, "Retta Interpolante", "L");
-    legend->SetTextSize(0.04);
-    legend->SetBorderSize(1);
-    legend->Draw();
+     TLegend *legend = new TLegend(0.15, 0.15, 0.5, 0.3);
+     legend->AddEntry(fileInput, "Dati con errore", "P");
+     legend->AddEntry(retta, "Retta Interpolante", "L");
+     legend->SetTextSize(0.04);
+     legend->SetBorderSize(1);
+     legend->Draw();
 
-    //Inizio residui
-    vector<double> scarti;
-    run_test_lineare(scarti, vin, vout, fit1.a[0], fit1.b[0]);
+     //Inizio residui
+     vector<double> scarti;
+     run_test_lineare(scarti, vin, vout, fit1.a[0], fit1.b[0]);
 
-    auto canvas2 = new TCanvas("c2", "Scarti circuito 1", 1100, 600);
-    canvas2->SetGrid();
-    canvas2->SetFillColor(0);
-    TGraphErrors *graph_scarti = new TGraphErrors(into_root(vin), into_root(scarti), into_root(err_vin), into_root(err_vout));
+     auto canvas2 = new TCanvas("c2", "Scarti circuito 1", 1100, 600);
+     canvas2->SetGrid();
+     canvas2->SetFillColor(0);
+     TGraphErrors *graph_scarti = new TGraphErrors(into_root(vin), into_root(scarti), into_root(err_vin), into_root(err_vout));
 
-    graph_scarti->SetMarkerColor(4);
-    graph_scarti->SetLineColor(kAzure - 3);
-    graph_scarti->SetMarkerStyle(20);
-    graph_scarti->SetMarkerSize(0.7);
-    graph_scarti->SetTitle("");
-    graph_scarti->GetXaxis()->SetTitle("Vpp_{IN} [mV]");
-    graph_scarti->GetYaxis()->SetTitle("Scarti");
-    graph_scarti->GetXaxis()->SetAxisColor(14);
-    graph_scarti->GetYaxis()->SetAxisColor(14);
+     graph_scarti->SetMarkerColor(4);
+     graph_scarti->SetLineColor(kAzure - 3);
+     graph_scarti->SetMarkerStyle(20);
+     graph_scarti->SetMarkerSize(0.7);
+     graph_scarti->SetTitle("");
+     graph_scarti->GetXaxis()->SetTitle("Vpp_{IN} [mV]");
+     graph_scarti->GetYaxis()->SetTitle("Scarti");
+     graph_scarti->GetXaxis()->SetAxisColor(14);
+     graph_scarti->GetYaxis()->SetAxisColor(14);
 
-    graph_scarti->Draw("AP");
+     graph_scarti->Draw("AP");
 
-    TLegend *legend2 = new TLegend(0.15, 0.15, 0.5, 0.3);
-    legend2->AddEntry(fileInput, "Dati con errore", "P");
-    legend2->SetTextSize(0.04);
-    legend2->SetBorderSize(1);
-    legend2->Draw();
+     TLegend *legend2 = new TLegend(0.15, 0.15, 0.5, 0.3);
+     legend2->AddEntry(fileInput, "Dati con errore", "P");
+     legend2->SetTextSize(0.04);
+     legend2->SetBorderSize(1);
+     legend2->Draw();
 
-    //------------------------------------------FINE GRAFCIO E CALCOLO PARAMETRI---------------
+     //------------------------------------------FINE GRAFCIO E CALCOLO PARAMETRI---------------
 
-    double ampl_sper = -1. * fit1.b[0] * 1e3;
-    double err_ampl_sper = fit1.err_b[0] * 1e3;
-    cout << "Amplificazione calcolata:" << endl
-         << ampl_sper << "+-" << err_ampl_sper << endl
-         << endl;
+     double ampl_sper = -1. * fit1.b[0] * 1e3;
+     double err_ampl_sper = fit1.err_b[0] * 1e3;
+     cout << "Amplificazione calcolata:" << endl
+          << ampl_sper << "+-" << err_ampl_sper << endl
+          << endl;
 
-    cout << "Compatibilità ampli th e calcolata: " << comp(ampl_th, ampl_sper, err_ampl_sper, err_ampl_th) << endl
-         << endl;
+     cout << "Compatibilità ampli th e calcolata: " << comp(ampl_th, ampl_sper, err_ampl_sper, err_ampl_th) << endl
+          << endl;
 
-    for (int i = 0; i < err_vin.size(); i++)
-    {
-        cout << vin[i] << " +/- " << err_vin[i] << "  " << fs_vin[i] << "  " << vout[i] << " +/- " << err_vout[i] << "  " << fs_vin[i] << endl;
-    }
+     for (int i = 0; i < err_vin.size(); i++)
+     {
+          cout << vin[i] << " +/- " << err_vin[i] << "  " << fs_vin[i] << "  " << vout[i] << " +/- " << err_vout[i] << "  " << fs_vin[i] << endl;
+     }
 }
